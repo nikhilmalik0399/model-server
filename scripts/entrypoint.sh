0134e7e506a7efc760a30e22a481dfd0e0ef7bd3
nikhilmalik0399 <nikhilmalik0399@gmail.com>
2026-04-29 21:03:52 +0530

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,4 @@ if [ -z "$PROFILE" ]; then
   export PROFILE=balanced
 fi
 
-python app/main.py &
-
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
